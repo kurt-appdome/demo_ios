547cbbc26c8146dff9518910082431f7633242d2
kurtdusek <kurt@appdome.com>
2023-05-25 07:54:42 -0600

    Initial Commit

---
+++ demo_app/demo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_appApp.swift
+//  demo_app
+//
+//  Created by Kurt Dusek on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct demo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
